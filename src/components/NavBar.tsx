3ccf163ab99290e873797efb6096a041ab72d95a
Maz <mazspm@icloud.com>
2023-05-02 17:59:53 -0500

    cleaned up the styling 🧼💅

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,44 +6,26 @@ export const NavBar = () => {
   const { isLoaded: userLoaded, isSignedIn } = useUser();
   if (!userLoaded) return <div />;
   return (
-    <nav className="border-gray-200 bg-white dark:bg-gray-900">
+    <nav className="bg-[#1a34d3]">
       <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
         <Link href="/" className="flex items-center">
           <Image
-            src="/public/koi.png"
+            src=""
             className="mr-3 h-8"
             alt="Logo"
             width={32}
             height={32}
           />
-          <span className="self-center whitespace-nowrap text-2xl font-semibold dark:text-white">
+          <span className="self-center whitespace-nowrap text-2xl font-semibold">
             Koi
           </span>
         </Link>
-        <button
-          data-collapse-toggle="navbar-default"
-          type="button"
-          className="ml-3 inline-flex items-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600 md:hidden"
-          aria-controls="navbar-default"
-          aria-expanded="false"
-        >
-          <span className="sr-only">Open main menu</span>
-          <svg
-            className="h-6 w-6"
-            aria-hidden="true"
-            fill="currentColor"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"></path>
-          </svg>
-        </button>
         <div className="hidden w-full md:block md:w-auto">
-          <ul className="mt-4 flex flex-col rounded-lg border border-gray-100 bg-gray-50 p-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0 md:bg-white md:p-0 md:dark:bg-gray-900">
+          <ul className="mt-4 flex flex-col border p-4 font-medium md:mt-0 md:flex-row md:space-x-8 md:border-0 md:p-0">
             <li>
               <Link
                 href="/"
-                className="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
+                className="block py-2 pl-3 pr-4 md:p-0"
                 aria-current="page"
               >
                 Home
@@ -52,7 +34,7 @@ export const NavBar = () => {
             <li>
               <Link
                 href="/sell-an-item"
-                className="block rounded bg-blue-700 py-2 pl-3 pr-4 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500"
+                className="block py-2 pl-3 pr-4 md:p-0"
                 aria-current="page"
               >
                 Sell
@@ -62,7 +44,7 @@ export const NavBar = () => {
               <>
                 <li>
                   <SignInButton>
-                    <span className=" block cursor-pointer rounded bg-blue-700 py-2 pl-3 pr-4 text-white dark:text-white md:bg-transparent md:p-0 md:text-blue-700 md:dark:text-blue-500">
+                    <span className="block cursor-pointer py-2 pl-3 pr-4 md:p-0">
                       Sign in
                     </span>
                   </SignInButton>
