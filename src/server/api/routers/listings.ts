100df5a05c91cd18adf4b947a293774f30d2ea5b
Maz <mazspm@icloud.com>
2023-05-03 17:01:34 -0500

    Revert "adding endpoint to fetch all listing"
    This reverts commit 286c8225c3b5d17fd9beb799e791e96d8014e747.

--- src/server/api/routers/listings.ts
+++ src/server/api/routers/listings.ts
@@ -1,16 +1,8 @@
 import { z } from "zod";
 
-import {
-  createTRPCRouter,
-  protectedProcedure,
-  publicProcedure,
-} from "~/server/api/trpc";
+import { createTRPCRouter, protectedProcedure } from "~/server/api/trpc";
 
 export const listingRouter = createTRPCRouter({
-  getListings: publicProcedure.query(({ ctx }) => {
-    // TODO: fetch from database
-    return ctx.prisma.listing.findMany();
-  }),
   create: protectedProcedure
     .input(
       z.object({ name: z.string(), description: z.string(), price: z.number() })
